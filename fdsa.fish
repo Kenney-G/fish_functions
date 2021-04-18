feffe1797a353e6a9b9ba74d4feaea2e9420a1f9
kendofriendo <goadke@g.cofc.edu>
2021-04-18 13:50:46 -0400

    Update fdsa.fish

--- fdsa.fish
+++ fdsa.fish
@@ -1,9 +1,10 @@
-#asdf version manager script - add a plugin, install latest, and set its global version
-
-function fdsa
-    asdf plugin add $argv;
-    asdf install $argv latest;
-    asdf global $argv (asdf latest $argv);
-    asdf reshim $argv;
-    echo "latest $argv version has been installed and set globally."
+function fdsa --description 'Script for asdf to add a plugin, install the latest version, and the version globally.'
+    if asdf plugin add $argv
+        asdf install $argv latest
+        asdf global $argv (asdf latest $argv)
+        asdf reshim $argv
+            set_color -i green; echo "fdsa: $argv was installed, set to latest version globally, and is ready to use."
+    else
+        set_color -i yellow;  echo 'fdsa: Please check the number and dial your call again.'
+    end
 end
